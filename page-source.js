a92658be65565232607c9adb3e9ba31313bbba48
liam4 <towerofnix@gmail.com>
2016-01-30 14:43:24 -0400

    gh-pages stuff should stay on gh-pages
    Less clutter

--- page-source.js
+++
@@ -1,6 +0,0 @@
-fetch('index.html')
-  .then(function(r) {return r.text()})
-  .then(function(t) {
-    document.querySelector('pre > code').appendChild(document.createTextNode(t));
-    Prism.highlightAll();
-  });
